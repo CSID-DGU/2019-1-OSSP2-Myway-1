b675557a29d65eb0b249f2de8e6ce5e6b1f077b6
lee minju <dptmfkdls120@gmail.com>
2019-05-06 16:05:57 +0900

    #3
    back button, alert

--- support_pro/src/app/login/login.page.ts
+++ support_pro/src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 import { NavController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -16,23 +17,29 @@ export class LoginPage implements OnInit {
   // tslint:disable-next-line:no-inferrable-types
   password: string = '';
 
-  constructor(public navCtrl: NavController, public afAuth: AngularFireAuth) { }
+  constructor(public navCtrl: NavController, public afAuth: AngularFireAuth, private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
-
   async login() {
     const { username, password } = this;
     try {
         const res = await this.afAuth.auth.signInWithEmailAndPassword(username, password);
-        console.log('login succeed');
         this.navCtrl.navigateBack('/tabs/tab1');
       } catch (err) {
-      console.dir(err);
-      console.log('Check id or password');
-      if (err.code === 'auth/user-not-found') {
-        console.log('User not found');
-      }
+      this.alertCtrl.create({
+        header: '',
+        message: '빈곳을 확인해주세요',
+        buttons: [{
+          text: '확인',
+          role: 'cancel'
+        }]
+      }).then(alertEl => {
+        alertEl.present();
+      });
     }
   }
 }
+
+
+  
--- support_pro/src/app/register/register.page.ts
+++ support_pro/src/app/register/register.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 import { NavController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -16,21 +17,43 @@ export class RegisterPage implements OnInit {
   password: string = '';
   // tslint:disable-next-line:no-inferrable-types
   cpassword: string = '';
-  constructor(public navCtrl: NavController, public afAuth: AngularFireAuth) { }
+  constructor(public navCtrl: NavController, public afAuth: AngularFireAuth,private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
   async register() {
     const { username, password, cpassword } = this;
+
     if (password !== cpassword) {
-      return console.error('Passwords don\'t match');
+      return this.alertCtrl.create({
+        header: '',
+        message: '비밀번호가 다릅니다',
+        buttons: [{
+          text: '확인',
+          role: 'cancel'
+        }]
+      }).then(alertEl => {
+        alertEl.present();
+      });
+    }
+    if(username === "" || password === ""){
+      this.alertCtrl.create({
+        header: '',
+        message: '아이디와 비밀번호를 확인해주세요',
+        buttons: [{
+          text: '확인',
+          role: 'cancel'
+        }]
+      }).then(alertEl => {
+        alertEl.present();
+      });
+      return;
     }
     try {
       const res =  this.afAuth.auth.createUserWithEmailAndPassword(username, password);
-      console.log(res);
       this.navCtrl.navigateBack('/tabs/tab1');
   } catch (error) {
-        console.dir(error);
+   
     }
   }
 
