a6ba216a22cbbe12956902b579ef30bf6bc93d49
una97 <yoona607@dongguk.edu>
2019-05-10 12:05:25 +0900

    db에서 가져오는거 test
    db에서 가져오는거 test

--- support_pro/src/app/tab1/tab1.page.ts
+++ support_pro/src/app/tab1/tab1.page.ts
@@ -1,21 +1,38 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, AlertController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { Platform } from '@ionic/angular';
 import {ActivatedRoute} from '@angular/router';
-
+import {AngularFireDatabase,FirebaseListObservable}from 'angularfire2/database';
+//import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+
+  //Items:any;
+  // constructor(public navCtrl:NavController,public alert:AlertController,private db:AngularFireDatabase){
+  //   this.Items = this.db.list('data');
+  // }
+
+  // user ={
+  //   firstname:"",
+  //   secondname:""
+  // }
+  // add(User){
+  //   firebase.database().ref('user').push(User);
+  // }
+public Items:any;
   public userid: string;
   constructor(
     public plat: Platform,
     public stor: Storage,
     public activatedRoute: ActivatedRoute,
-    public navCtrl: NavController
+    public navCtrl: NavController,
+    public db:AngularFireDatabase
     ) {
       this.userid = this.activatedRoute.snapshot.paramMap.get('userid');
       this.plat.ready().then(() => {
@@ -23,6 +40,7 @@ export class Tab1Page {
           this.userid = val;
         });
       });
+      this.Items=this.db.list('data');
     }
   logout() {
     this.userid = null;
