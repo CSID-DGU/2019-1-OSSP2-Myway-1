d78e46684444a3b54425f20efa26fbd13ad8c942
lee minju <dptmfkdls120@gmail.com>
2019-05-17 19:01:15 +0900

    #4
    change chat logic & UI

--- support_pro/src/app/app.component.ts
+++ support_pro/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+declare var window;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -20,6 +20,20 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      window.ApiAIPlugin.init(
+        {
+            clientAccessToken: '52789103d0e941aa863af61d4938a331', // insert your client access key here
+            lang: 'ko' // set lang tag from list of supported languages
+        },
+// tslint:disable-next-line: only-arrow-functions
+        function(result) {
+          alert(result)
+         },
+// tslint:disable-next-line: only-arrow-functions
+        function (error) {
+          alert(error)
+          }
+    );
     });
   }
 }
--- support_pro/src/app/tab2/tab2.page.ts
+++ support_pro/src/app/tab2/tab2.page.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, NgZone } from '@angular/core';
+import { IonContent, NavController } from '@ionic/angular';
 
+declare var ApiAIPromises: any;
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  Delete() {}
+  messages: any[] = [];
+  text: string;
+
+  @ViewChild(IonContent) content: IonContent;
+  //@ViewChild(Content) contentArea: Content;
+
+  constructor(public navCtrl: NavController, public ngZone: NgZone) {
+    this.messages.push({
+      text: '왜 왔는가?',
+      sender: 'api',
+      createAt: new Date().getTime()
+    });
+  }
+
+
+  ask() {
+    if (this.text !== '') {
+    this.messages.push({
+      text: this.text,
+      sender: 'me',
+      createAt: new Date().getTime()
+    });
+    setTimeout(() => {
+      this.content.scrollToBottom(200);
+    });
+    this.goToDia(this.text);
+    this.text = '';
+  }
+  }
+  goToDia( str: string) {
+    ApiAIPromises.requestText({
+      query: this.text
+    })
+    .then(({result: {fulfillment: {speech}}}) => {
+       this.ngZone.run(() => {
+        // this.messages = speech;
+        this.messages.push({
+          text: speech,
+          sender : 'api',
+          createAt: new Date().getTime()
+        });
+        setTimeout(() => {
+          this.content.scrollToBottom(200);
+        });
+       });
+    });
+  }
 }
