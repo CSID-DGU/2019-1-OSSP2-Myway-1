af67981c56a536c97fa52f1e35ca49ad725e96e3
ha-yujin <hoj2887@dongguk.edu>
2019-06-10 19:08:46 +0900

    chatting

--- support_pro/src/app/chat-view/chat-view.page.ts
+++ support_pro/src/app/chat-view/chat-view.page.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase';
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'app-chat-view',
   templateUrl: './chat-view.page.html',
@@ -12,10 +14,17 @@ export class ChatViewPage implements OnInit {
 
   text:string;
   chatRef:any;
-  uid:string;
-
-  constructor(public af:AngularFireAuth,public fs:AngularFirestore) { 
-    this.uid=localStorage.getItem('userid');
+  me:string;
+  you:string;
+  Email:string;
+  constructor(public af:AngularFireAuth,public fs:AngularFirestore, public stor:Storage) { 
+    this.stor.get('me').then((val)=>{
+      this.me=val
+    });
+    this.stor.get('you').then((val)=>{
+      this.you=val
+    });
+    this.Email=this.af.auth.currentUser.email;
     this.chatRef=this.fs.collection('chats',ref=>ref.orderBy('Timestamp')).valueChanges();
   }
 
@@ -26,8 +35,8 @@ export class ChatViewPage implements OnInit {
     if(this.text!=''){
       this.fs.collection('chats').add({
         Email:this.af.auth.currentUser.email,
+        You:this.you,
         Message:this.text,
-        UserID:this.af.auth.currentUser.uid,
         Timestamp:firebase.firestore.FieldValue.serverTimestamp(),
       });
       this.text='';
--- support_pro/src/app/tab4/tab4.page.ts
+++ support_pro/src/app/tab4/tab4.page.ts
@@ -3,6 +3,7 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { NavController } from '@ionic/angular';
 import * as firebase from 'firebase';
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
@@ -13,21 +14,14 @@ export class Tab4Page {
   chattingRef:any;
   Email:string;
 
-  constructor(public fs:AngularFirestore,public af:AngularFireAuth, public nav:NavController ){
+  constructor(public fs:AngularFirestore,public af:AngularFireAuth, public nav:NavController, public stor:Storage ){
 
-    this.Email=this.af.auth.currentUser.email;
-    /*this.fs.collection('chatting').add({ // 대화한 상대목록  저장
-      you: "yoo@naver.com",
-      me:this.Email,
-      Timestamp:firebase.firestore.FieldValue.serverTimestamp()
-    })
-    //this.uid=localStorage.getItem('userid');
-    */
+    this.Email=this.af.auth.currentUser.email,
     this.chattingRef=this.fs.collection('chatting',ref=>ref.orderBy('Timestamp')).valueChanges();
-
-    //this.nav.navigateForward('/chat-view');
   }
-  select(){
+  openChat(){
+    this.stor.set('me', this.Email);
+    this.stor.set('you',"yoo@naver.com");
     this.nav.navigateRoot('/chat-view');
   }
 }
