802b2df4f5a1a44755dea0e9ff9bb224753ec11d
ha-yujin <hoj2887@dongguk.edu>
2019-06-12 00:47:02 +0900

    오류수정

--- support_pro/src/app/tab4/tab4.page.ts
+++ support_pro/src/app/tab4/tab4.page.ts
@@ -26,9 +26,9 @@ export class Tab4Page {
     this.Email=this.af.auth.currentUser.email,
     this.chattingRef=this.fs.collection('chatting',ref=>ref.orderBy('Timestamp')).valueChanges();
   
-    /*this.fs.collection('chattting').add({
-      me:this.af.auth.currentUser.email,
-      you:this.you,
+    /*this.fs.collection('chatting').add({
+      uid1:this.af.auth.currentUser.email,
+      uid2:"hoj2887@gmail.com",
       Timestamp:firebase.firestore.FieldValue.serverTimestamp(),
     });*/
   }
