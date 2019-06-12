db16ed7b0a18ffdc28fa7e7f3e467d31207190a2
lee minju <dptmfkdls120@gmail.com>
2019-06-13 00:22:53 +0900

    복구
    복구

--- support_pro/src/app/post/post.page.ts
+++ support_pro/src/app/post/post.page.ts
@@ -4,6 +4,10 @@ import { Platform } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
 import {ActivatedRoute, Router} from '@angular/router';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { AlertController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 import { trigger, state, style, animate, transition } from '@angular/animations';
 
 
@@ -45,68 +49,84 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 
 })
 export class PostPage implements OnInit {
+  public title: string;
+  item: any;
+  Email: string;
+  chattingRef: any;
   public userid: string;
-public title:string;
-item:any;
 
 public likeState: string = 'unliked';
+    constructor(
+      public plat: Platform,
+      public activatedRoute: ActivatedRoute,
+      public navCtrl: NavController,
+      public db: AngularFireDatabase,
+      public atrCtrl: AlertController,
+      public fs: AngularFirestore,
+      public af: AngularFireAuth,
+      public router: Router) {
+        // this.item = this.activatedRoute.snapshot.paramMap.get('title');
+        // console.log(this.item);
+      this.title = this.activatedRoute.snapshot.paramMap.get('title');
+      this.load();
+      }
 public iconName: string = 'heart-empty';
-
 public scrapState: string = 'unscrap';
 public starName: string = 'star-outline';
-
-
-
-
-  constructor( 
-    public plat: Platform,
-    public activatedRoute: ActivatedRoute,
-    public navCtrl: NavController,
-    public db: AngularFireDatabase,
-    public router:Router) 
-    { 
-      // this.item = this.activatedRoute.snapshot.paramMap.get('title');
-      // console.log(this.item);
-      this.userid=this.activatedRoute.snapshot.paramMap.get('userid');
-    this.title= this.activatedRoute.snapshot.paramMap.get('title');
-    this.load();
-
-}
-
-    load(){
+    load() {
       this.db.list('regisTxt/', ref => ref.orderByChild('title').equalTo(this.title)).valueChanges().subscribe(
         data => {
         this.item = data;
-        });  
+        });
     }
-  
-    consolePrint(){
-     
+    async gotoChat(you: string) {
+      const alert = await this.atrCtrl.create({
+          header: 'Confirm!',
+          message: '<strong>' + you + '</strong>' + '와 채팅하시겠습니까??',
+          buttons: [
+            {
+              text: 'Cancel',
+              role: 'cancel',
+              cssClass: 'secondary',
+              handler: (blah) => {
+                console.log('Confirm Cancel: blah');
+              }
+            }, {
+              text: 'Okay',
+              handler: () => {
+                console.log('Confirm Okay');
+                this.chattingRef = this.fs.collection('chatting', ref => ref.orderBy('Timestamp')).valueChanges();
+                this.fs.collection('chatting').add({
+                  uid1: this.af.auth.currentUser.email,
+                  uid2: you,
+                  Timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                });
+                this.router.navigate(['chat-view', you]);
+              }
+            }
+          ]
+        });
+      await alert.present();
     }
-    toggleLikeState(){
-
-      if(this.likeState == 'unliked'){
+    toggleLikeState() {
+      if (this.likeState === 'unliked') {
         this.likeState = 'liked';
         this.iconName = 'heart';
       } else {
         this.likeState = 'unliked';
         this.iconName = 'heart-empty';
       }
-  
     }
 
-    toggleScrapState(){
-
-      if(this.scrapState == 'unscrap'){
+    toggleScrapState() {
+      if (this.scrapState === 'unscrap') {
         this.scrapState = 'scrap';
         this.starName = 'star';
       } else {
         this.scrapState = 'unscrap';
         this.starName = 'star-outline';
       }
-  
     }
-  
   ngOnInit() {
   }
 
