76062f9b7250440e30a2c8aa6128127a449136d5
Soyoung Yoo <yooso020599@dongguk.edu>
2019-06-18 18:53:29 +0900

    check

--- support_pro/src/app/tab3/tab3.page.ts
+++ support_pro/src/app/tab3/tab3.page.ts
@@ -17,12 +17,8 @@ export class Tab3Page {
   contentImg;
   download;
   pictureRef;
-<<<<<<< HEAD
   // tslint:disable-next-line:max-line-length
   picname: string = 'https://firebasestorage.googleapis.com/v0/b/supportingbot.appspot.com/o/userimg%2F%EB%8B%A4%EC%9A%B4%EB%A1%9C%EB%93%9C.png?alt=media&token=f9e09289-ae6f-4368-858c-74c46ae5242b';
-=======
-  picname:string='';
->>>>>>> ce90b117ee300682fe556223224d13710b21c2be
   imageURI;
   hcount = 0; // 해시태그 여러개 입력한 경우를 위한 카운트
   // tslint:disable-next-line: variable-name
@@ -143,11 +139,8 @@ export class Tab3Page {
         return 0;
       }
       if (this.titleInput === '' || this.majorInput === '' || this.classInput === '' || this.profInput === '' ||
-<<<<<<< HEAD
-         this.sdateInput === '' || this.edateInput === '' || this.conInput === '' || this.hashtag === [] ) {
-=======
+         // tslint:disable-next-line:max-line-length
          this.sdateInput === '' || this.edateInput === '' || this.con1 === '' ||this.con2 === '' ||this.con3 === '' ||this.con4 === '' || this.hashtag === []) {
->>>>>>> ce90b117ee300682fe556223224d13710b21c2be
           this.alertCtrl.create({
             header: '',
             message: '내용을 전부 입력해주세요',
@@ -169,7 +162,7 @@ export class Tab3Page {
         this.regisTxt.prof = this.profInput;
         this.regisTxt.sdate = this.sdateInput;
         this.regisTxt.edate = this.edateInput;
-        //this.regisTxt.content = this.conInput.replace('\n', '<br>');
+        // this.regisTxt.content = this.conInput.replace('\n', '<br>');
         this.regisTxt.con1=this.con1.replace('\n','<br>');
         this.regisTxt.con2=this.con2;
         this.regisTxt.con3=this.con3.replace('\n','<br>');
@@ -188,15 +181,10 @@ export class Tab3Page {
           console.log(val, this.tmp_hash_1, this.hash_1);
           // 전체 게시글 저장
           this.db.object(`regisTxt/${this.tmp_hash_1}`).set(this.regisTxt);
-<<<<<<< HEAD
           // tslint:disable-next-line:max-line-length
           if (this.regisTxt.img !== 'https://firebasestorage.googleapis.com/v0/b/supportingbot.appspot.com/o/userimg%2F%EB%8B%A4%EC%9A%B4%EB%A1%9C%EB%93%9C.png?alt=media&token=f9e09289-ae6f-4368-858c-74c46ae5242b') {
           this.showImage();
           }
-=======
-          if(this.regisTxt.img!='')
-            this.showImage();
->>>>>>> ce90b117ee300682fe556223224d13710b21c2be
           this.tmp_hash_1 += 1;
           console.log('전체 게시글?' + this.classInput, this.tmp_hash_1);
           // 전체 게시글 수 저장
