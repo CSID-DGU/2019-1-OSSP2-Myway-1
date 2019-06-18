a20cc0ca0da5591de65f97c1fcd32871100ce8a6
ha-yujin <hoj2887@dongguk.edu>
2019-06-18 15:54:54 +0900

    session

--- support_pro/src/app/tab5/tab5.page.ts
+++ support_pro/src/app/tab5/tab5.page.ts
@@ -22,21 +22,12 @@ export class Tab5Page {
     public router: Router,
     public atrCtrl: AlertController
     ) {
-     // this.userid = this.activatedRoute.snapshot.paramMap.get('userid');
     }
-<<<<<<< HEAD
     ionViewWillEnter() {
       this.stor.get('id').then((val) => {
         this.userid = val;
       });
     }
-=======
-  ionViewWillEnter(){
-    this.stor.get('id').then((val) => {
-      this.userid = val;
-    });
-  }
->>>>>>> 4e45568cfd19130fda58dab7e06a489d8db78076
   async atrLout() {
     const alert = await this.atrCtrl.create({
       header: '확인',
